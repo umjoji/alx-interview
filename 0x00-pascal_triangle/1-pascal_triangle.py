59f58c1faaa2bf6290f4a4f4ec43372b6810b510
umjoji <30466162+umjoji@users.noreply.github.com>
2023-07-06 17:29:56 +0100

    Add other solution for Pascal triangle

---
+++ 0x00-pascal_triangle/1-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """A function that returns a list of integers
+    representing Pascal triangle of n"""
+    p_list = []
+    if n <= 0:
+        return p_list
+    for i in range(1, n+1):
+        c = 1
+        q_list = []
+        q_list.append(c)
+        for j in range(1, i+1):
+            c = c * (i - j) // j
+            if c != 0:
+                q_list.append(c)
+        p_list.append(q_list)
+
+    return p_list
