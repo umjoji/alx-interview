0a75f01b25608774ef8d5f5faf6da2efd64f7e87
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2023-08-30 10:28:04 +0100

    Annotate function and rename variables

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,10 +3,10 @@
     determine the fewest number of coins needed to meet
     a given amount total.
 '''
-import sys
+from typing import List
 
 
-def makeChange(coins, total):
+def makeChange(coins: List[int], total: int) -> int:
     '''
     Return: fewest number of coins needed to get
     If total is 0 or less, return 0
@@ -14,16 +14,16 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
+    table = [float('inf')] * (total + 1)
     table[0] = 0
     available_denominations = len(coins)
     for i in range(1, total + 1):
         for j in range(available_denominations):
             if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
+                min_coins_for_remainder = table[i - coins[j]]
+                if min_coins_for_remainder != float('inf') and min_coins_for_remainder + 1 < table[i]:
+                    table[i] = min_coins_for_remainder + 1
 
-    if table[total] == sys.maxsize:
+    if table[total] == float('inf'):
         return -1
     return table[total]
